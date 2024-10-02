5656a55e68e5827407228ae3f7aa871e2b7aaae0
roguetitan1703 <omchandel1703@gmail.com>
2024-10-02 12:53:08 +0530

    secy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,17 +16,17 @@ const Hero = () => {
           </h1>
         </div>
 
-        <div className="max-w-3xl flex flex-row justify-between items-center mt-12 z-0">
+        <div className="max-w-3xl flex flex-row justify-between items-start mt-12 z-0">
           <p className="text-xl">
             to Design, Build, Plan and Execute your next idea into Reality
           </p>
           <a href="#" className="flex items-center group z-10">
             <div className="relative rounded-full flex flex-row items-center px-4 py-2 bg-gradient-to-l hover:bg-none from-cyan-400 to-purple-500 hover:ring-white hover:ring-1 hover:scale-105 transition-transform ease-in-out duration-150">
-              <span className="mr-1 text-transparent bg-clip-text bg-white group-hover:bg-gradient-to-r from-cyan-400 to-purple-500 group-hover:text-transparent">
+              <span className="mr-1 text-transparent text-nowrap bg-clip-text bg-white group-hover:bg-gradient-to-r from-cyan-400 to-purple-500 group-hover:text-transparent">
                 Get in touch
               </span>
               <span className="inline-block transform transition-transform duration-300 ease-in-out -translate-x-2 group-hover:translate-x-2">
-                <FontAwesomeIcon icon={faArrowRight} className="" />
+                <FontAwesomeIcon icon={faArrowRight} className="ml-2" />
               </span>
             </div>
           </a>
